8e88aa0cb46d0bdf1d6206b1a3ead548895cf46c
hanshuaile <hanshuaile@100tal.com>
2019-04-03 19:22:58 +0800

    83. 删除排序链表中的重复元素

---
+++ 83.删除排序链表中的重复元素.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=javascript
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (44.75%)
+ * Total Accepted:    21.5K
+ * Total Submissions: 48K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if (!head) return head
+    let p = head
+    let q = head.next
+    while (q) {
+        if (p.val === q.val) {
+            p.next = q.next
+            q = p.next
+        } else {
+            q = q.next
+            p = p.next
+        }
+    }
+    return head
+};
+
