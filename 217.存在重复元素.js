81ba776a3f51f9c94b79637235fbb582a4543f1e
simple <807567924@qq.com>
2019-10-09 18:13:44 +0800

    217. 存在重复元素

--- 217.存在重复元素.js
+++ 217.存在重复元素.js
@@ -26,24 +26,32 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    /*
-        方法二
-        先对数组进行排序，两两比较
-        如果 nums[i] === nums[i + 1], 说明有相同元素， 返回true
-        如果循环能正常执行完成，则说明没有相同元素， 返回false
+  /*
+    方法三
 
-        时间复杂度 排序算法应该是O(n^2)， for循环O(n)， 所以为O(n^2)
+    利用set对象
+    遍历nums
+    逐个判断该元素在不在set中， 如果已经存在， 说明存在重复元素，返回true
+    否则，将该元素添加到set中
 
-        Accepted
-        18/18 cases passed (116 ms)
-        Your runtime beats 44.83 % of javascript submissions
-        Your memory usage beats 65.57 % of javascript submissions (38.4 MB)
-    */
-    nums.sort()
-    for(let i = 0, len = nums.length; i < nums.length - 1; i++) {
-        if(nums[i] === nums[i + 1]) return true
+    如果循环完成，都没有return true， 说明不存在重复元素，返回false
+
+    时间复杂度 O(n)
+    空间复杂度 O(n)
+
+    Accepted
+    18/18 cases passed (76 ms)
+    Your runtime beats 94.97 % of javascript submissions
+    Your memory usage beats 45.43 % of javascript submissions (40.1 MB)
+  */
+  let set = new Set()
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (set.has(nums[i])) return true
+    else {
+      set.add(nums[i])
     }
-    return false
+  }
+  return false
 };
 
 /*
@@ -60,6 +68,27 @@ var containsDuplicate = function(nums) {
     18/18 cases passed (76 ms)
     Your runtime beats 95.05 % of javascript submissions
     Your memory usage beats 8.55 % of javascript submissions (43.4 MB)
+
+
+    方法二
+    先对数组进行排序，两两比较
+    如果 nums[i] === nums[i + 1], 说明有相同元素， 返回true
+    如果循环能正常执行完成，则说明没有相同元素， 返回false
+
+    时间复杂度 排序算法应该是O(n^2)， for循环O(n)， 所以为O(n^2)
+
+    Accepted
+    18/18 cases passed (116 ms)
+    Your runtime beats 44.83 % of javascript submissions
+    Your memory usage beats 65.57 % of javascript submissions (38.4 MB)
+
+    nums.sort()
+    for(let i = 0, len = nums.length; i < nums.length - 1; i++) {
+        if(nums[i] === nums[i + 1]) return true
+    }
+    return false
+
+
 */
 // @lc code=end
 
